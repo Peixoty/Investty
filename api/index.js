175d31570b2d78d27aba9aed5d5263f48afbf1f8
Luiz Henrique <maestrellipeixoto@gmail.com>
2025-01-17 20:08:35 -0300

    API para ser rodada localmente

---
+++ api/index.js
@@ -0,0 +1,56 @@
+// Importação dos módulos
+// Import do express: ele roda o app
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+// Import do axios: Realiza requisições HTTP
+const axios = require("axios")
+
+// Import do cheerio: Manipula o HTML
+const cheerio = require("cheerio")
+
+// Importando o pacote CORS
+const cors = require('cors')
+//--------------------------------------------------------
+//Middleware Global
+
+// Habilitando o CORS para todas as origens
+app.use(cors())
+//--------------------------------------------------------
+
+
+app.get("/:ticker", async (req, res) => {
+    let { ticker } = req.params
+
+    // Verifica se o ticker não contém ":" (indicando que é um ticker da BVMF)
+    if(!ticker.includes(":")){
+        ticker = `${ticker}:BVMF`;
+    }
+
+    const url = `https://www.google.com/finance/quote/${ticker}`
+
+    try {
+        const { data } = await axios.get(url)
+        const $ = cheerio.load(data)
+
+        const stockName = $('.zzDege').text() // Nome da ação
+        const stockPrice = $('.YMlKec.fxKbKc').first().text() // Preço atual
+        const stockClose = $('.P6K39c').first().text(); // Valor de fechamento
+
+        if (!stockName || !stockPrice) return res.status(404).json({ error: "Ativo não encontrado" })
+
+        res.json({
+            ticker: ticker,
+            name: stockName,
+            price: stockPrice,
+            close: stockClose
+        })
+    } catch (error) {
+        console.error("Erro ao buscar dados de ativo: ", error.message)
+        res.status(500).json({ error: "Falhou ao buscar dados de ativo" })
+    }
+})
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
+
